d816e48b7f55df9d6141d5314332802c963d060b
genieva <gtcroley@gmail.com>
2023-06-07 22:15:51 -0600

    Update script.js
    Fixed syntax error on objects

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //An array for techniques
 let bjjTechniques = []
 //Adding objects to the array
-bjjTechniques = [profJohn = {name = 'John', price = 1, favTechnique = 'string'}, profAndrew = {name = 'Andrew', price = 1, faveTechnique = 'passing'}, profSofia= {} ]
+bjjTechniques = [profJohn = {name:'John', price: 1, favTechnique:  'string'}, profAndrew = {name: 'Andrew', price: 1, faveTechnique: 'passing'}, profSofia= {} ]
